a2de704a55b05d2aa90257ef03de7e9895d00895
Christopher <christopher.jaramillo@epn.edu.ec>
2021-07-25 07:16:13 -0500

    Examen Primer Bimestre - Christopher Jaramillo

---
+++ Examen/app/src/main/java/com/gracegbe/examen01/PaginaWeb.kt
@@ -0,0 +1,18 @@
+package com.gracegbe.examen
+
+import android.widget.ArrayAdapter
+import java.util.*
+
+class PaginaWeb (
+    var id_pagina: Int,
+    var id_servidor: Int,
+    var nombre: String,
+    var nombreIndex: String,
+    var autor: String,
+    var framework: String,
+    var lenguajes: String
+){
+    override fun toString():String{
+        return "${nombre}"
+    }
+}
