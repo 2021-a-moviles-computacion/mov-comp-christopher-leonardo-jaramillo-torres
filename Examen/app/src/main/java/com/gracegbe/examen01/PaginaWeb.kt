9385b5b540357c2dbaeb0c61eb9192b4f5d93c78
Christopher Jaramillo <christopher.jaramillo@epn.edu.ec>
2021-09-20 23:28:10 -0500

    Examen 1B

--- Examen/app/src/main/java/com/gracegbe/examen01/PaginaWeb.kt
+++ Examen/app/src/main/java/com/gracegbe/examen01/PaginaWeb.kt
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package com.gracegbe.examen
 
 import android.widget.ArrayAdapter
@@ -16,23 +15,3 @@ class PaginaWeb (
     override fun toString():String{
         return "${nombre}"
     }
-=======
-package com.gracegbe.examen
-
-import android.widget.ArrayAdapter
-import java.util.*
-
-class PaginaWeb (
-    var id_pagina: Int,
-    var id_servidor: Int,
-    var nombre: String,
-    var nombreIndex: String,
-    var autor: String,
-    var framework: String,
-    var lenguajes: String
-){
-    override fun toString():String{
-        return "${nombre}"
-    }
->>>>>>> Stashed changes
-}
--- Examen/app/src/main/java/com/gracegbe/examen01/Servidor.kt
+++ Examen/app/src/main/java/com/gracegbe/examen01/Servidor.kt
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package com.gracegbe.examen
 
 import java.util.*
@@ -19,28 +18,3 @@ class Servidor (
         return "${ubicacion}"
     }
 }
-
-=======
-package com.gracegbe.examen
-
-import java.util.*
-
-class Servidor (
-    var id_servidor: Int?,
-    var ubicacion: String?,
-    var direccionIP: String?,
-    var marca: String?,
-    var empresa: String?,
-    var tipoServidor: String?,
-    var protocolos: String?
-) {
-
-    var paginas = ArrayList<PaginaWeb>()
-
-    override fun toString():String{
-        return "${ubicacion}"
-    }
-}
-
->>>>>>> Stashed changes
-/*ubicacion, direccionIP, marca, empresa, tipoServidor, protocolos*/
